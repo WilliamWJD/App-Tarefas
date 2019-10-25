fe1973cd84afe3a9afd32a0c0e2c245a78e14053
williamwjd <william.jose.wjd@gmail.com>
2019-10-25 14:17:33 -0300

    Separando pesquisa de tarefa em um component

---
+++ mobile/src/components/Pesquisa/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native'
+
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+const Pesquisa = () => {
+    return (
+        <View style={styles.boxPesquisa}>
+            <View
+                style={styles.inputPesquisa}
+            >
+                <Icon
+                    name="search"
+                    size={30}
+                    color={'#95A5A6'}
+                    style={styles.iconPesq}
+                />
+                <TextInput
+                    placeholder="Encontre uma tarefa"
+                    placeholderTextColor="#95A5A6"
+                />
+            </View>
+            <TouchableOpacity
+                onPress={() => { }}
+                style={styles.add}
+            >
+                <Icon name="add" size={30} color={'white'} />
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#2980B9',
+        padding:20
+    },
+
+    boxPesquisa:{
+        flexDirection:'row'
+    },
+
+    inputPesquisa:{
+        flexDirection:'row',
+        backgroundColor:'#ECF0F1',
+        borderRadius:3,
+        width:'85%',
+        height:40,
+        alignItems:'center',
+    },
+
+    iconPesq:{
+        marginLeft:4
+    },
+
+    add:{
+        backgroundColor:'#2ECC71',
+        width:50,
+        height:40,
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:5,
+        marginLeft:10
+    }
+})
+
+export default Pesquisa
--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import Pesquisa from '../../components/Pesquisa'
 import ListaTarefas from '../../components/ListaTarefas'
 
 const Main = () => {
@@ -15,28 +16,7 @@ const Main = () => {
 
     return (
         <View style={styles.container}>
-            <View style={styles.boxPesquisa}>
-                <View
-                    style={styles.inputPesquisa}
-                >
-                    <Icon 
-                        name="search" 
-                        size={30} 
-                        color={'#95A5A6'}
-                        style={styles.iconPesq}
-                    />       
-                    <TextInput                      
-                        placeholder="Encontre uma tarefa"
-                        placeholderTextColor="#95A5A6"
-                    />
-                </View> 
-                <TouchableOpacity 
-                    onPress={()=>{}}
-                    style={styles.add}
-                >               
-                    <Icon name="add" size={30} color={'white'}/>
-                </TouchableOpacity>
-            </View>
+            <Pesquisa/>
             <ListaTarefas dados={dados}/>
         </View>
     )
@@ -47,33 +27,6 @@ const styles=StyleSheet.create({
         flex:1,
         backgroundColor:'#2980B9',
         padding:20
-    },
-
-    boxPesquisa:{
-        flexDirection:'row'
-    },
-
-    inputPesquisa:{
-        flexDirection:'row',
-        backgroundColor:'#ECF0F1',
-        borderRadius:3,
-        width:'85%',
-        height:40,
-        alignItems:'center',
-    },
-
-    iconPesq:{
-        marginLeft:4
-    },
-
-    add:{
-        backgroundColor:'#2ECC71',
-        width:50,
-        height:40,
-        alignItems:'center',
-        justifyContent:'center',
-        borderRadius:5,
-        marginLeft:10
     }
 })
 
